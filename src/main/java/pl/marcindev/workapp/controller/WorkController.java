1c9ba73bf897eaf535cfda47f09a3855b7f7c968
Marcin <gasiorek.mail@gmail.com>
2023-06-26 10:12:16 +0200

    add config server and new method

--- src/main/java/pl/marcindev/workapp/controller/WorkController.java
+++ src/main/java/pl/marcindev/workapp/controller/WorkController.java
@@ -2,9 +2,19 @@ package pl.marcindev.workapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WorkController {
+    @Value("${director.name}")
+    private String directorName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Team name: " + teamName + ", director name: " + directorName;
+    }
     @GetMapping("/start")
     public String start(){
         return "this is a starting point";
